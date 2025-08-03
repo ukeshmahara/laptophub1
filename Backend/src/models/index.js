0c8373a9dc71bcca20021b770b2704461dcc8d22
ukeshmahara <ukeshmahara361@gmail.com>
2025-08-03 17:16:26 +0545

    feat: organize routes and controllers

---
+++ Backend/src/models/index.js
@@ -0,0 +1,7 @@
+import { User } from "./user/User.js";
+import { Laptop } from "./laptop/Laptop.js";
+import { Order } from "./order/Order.js";
+import { OrderItem } from "./order/OrderItem.js";
+import { Wishlist } from "./wishlist/Wishlist.js";
+
+export { User, Laptop, Order, OrderItem, Wishlist };
---
+++ Backend/src/route/index.js
@@ -0,0 +1,15 @@
+import userRouter from "./user/userRoute.js";
+import authRouter from "./auth/authRoute.js";
+import laptopRouter from "./laptop/laptopRoute.js";
+import orderRouter from "./order/orderRoute.js";
+import wishlistRouter from "./wishlist/wishlistRoute.js";
+import uploadRouter from "./uploadRoutes.js";
+
+export {
+  userRouter,
+  authRouter,
+  laptopRouter,
+  orderRouter,
+  wishlistRouter,
+  uploadRouter
+};
